224ee341337be0d3048314ed9865a7f334f4426d
Thomas Di'Meco <thomas.dimeco@gmail.com>
2013-12-03 22:57:12 +0100

    PHPMyAdmin integration

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'nuitdelinfo'; // This password is useless! =D
+$db['default']['database'] = 'nuitdelinfo';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
