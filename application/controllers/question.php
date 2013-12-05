0f5915c2b6d9a513f29d7c27b2b6a67fb33e98d5
Laura Martellotto <laura.martellotto@gmail.com>
2013-12-06 00:52:20 +0100

    DBperia Spotlight service !

---
+++ application/controllers/question.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Question extends CI_Controller {
+
+    /**
+     * Question page
+     */
+    public function index ()
+    {
+        /* Load Models * /
+        $this->load->model('Questions_Model', 'Questions');
+        /* */
+
+        /* CURL */
+        $this->load->library('curl');
+        $this->curl->http_header('Accept: application/json');
+
+        // TODO
+        $url = urlencode('Je veux un aquarium avec un poisson');
+
+        $names = $this->curl->simple_get('http://spotlight.dbpedia.org/rest/spot/?text=' . $url);
+        $namesJSON = json_decode($names, true);
+        $tab = $namesJSON["annotation"]["surfaceForm"];
+
+        // Check is it's a associative tab (one result)
+        if (is_array($tab) && array_diff_key($tab,array_keys(array_keys($tab)))) {
+            echo $tab["@name"];
+        } else {
+            foreach($tab as $noun) {
+                echo $noun["@name"];
+            }
+
+        }
+
+
+
+    }
+}
---
+++ application/views/notification.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Laura
+ * Date: 06/12/2013
+ * Time: 00:54
+ */ 
